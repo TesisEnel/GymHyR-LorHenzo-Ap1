b54054716411ffc7b10163971aee9468a1cbb369
HenzoWTF <Urenahenzo@gmail.com>
2024-03-22 01:12:43 -0400

    Creacion del modelo productoDto

---
+++ Library/ProductoDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class ProductoDto
+    {
+        public int ProductoId { get; set; }
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+        public int cantidad { get; set; }
+
+    }
+}
