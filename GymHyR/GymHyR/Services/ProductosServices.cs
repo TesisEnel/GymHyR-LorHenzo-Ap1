b57505317d2cac9c4a2f6f39266fffa840e095c8
HenzoWTF <Urenahenzo@gmail.com>
2024-03-22 01:22:53 -0400

    ProductServices terminado

---
+++ GymHyR/GymHyR/Services/ProductosServices.cs
@@ -0,0 +1,75 @@
+﻿using GymHyR.Data;
+using GymHyR.Models;
+using Library;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymHyR.Services
+{
+    public class ProductosServices(ApplicationDbContext context)
+    {
+        public async Task<IEnumerable<ProductoDto>> GetProductos()
+        {
+            return await context.Productos
+                .Select(d => new ProductoDto()
+                {
+                    ProductoId = d.ProductoId,
+                    Nombre = d.Nombre,
+                    Precio = d.Precio,
+                    cantidad = d.cantidad
+                }).ToListAsync();
+        }
+
+        public async Task<Producto?> GetProducto(int id)
+        {
+            return await context.Productos.FindAsync(id);
+        }
+
+        public async Task<Producto> PostProducto(Producto producto)
+        {
+            context.Productos.Add(producto);
+            await context.SaveChangesAsync();
+            return producto;
+        }
+        public async Task<IActionResult> PutProducto(int id, Producto producto)
+        {
+            if (id != producto.ProductoId)
+            {
+                return new BadRequestResult();
+            }
+
+            context.Entry(producto).State = EntityState.Modified;
+
+            try
+            {
+                await context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductoExists(id))
+                {
+                    return new NotFoundResult();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return new NoContentResult();
+        }
+
+        public async Task<bool> DeleteProducto(Producto producto)
+        {
+            var cantidad = await context.Productos
+                .Where(c => c.ProductoId == producto.ProductoId)
+                .ExecuteDeleteAsync();
+
+            return cantidad > 0;
+        }
+        public bool ProductoExists(int id)
+        {
+            return context.Productos.Any(e => e.ProductoId == id);
+        }
+    }
+}
