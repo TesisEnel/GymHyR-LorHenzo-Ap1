3d9902c21ac1c84453e281cf8dcd16c573d3c7b0
HenzoWTF <Urenahenzo@gmail.com>
2024-03-26 22:52:58 -0400

    Todo desde cero, pero los login resuelto

---
+++ GymHyR/GymHyR/DAL/Contex.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GymHyR.DAL
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+    }
+}
--- GymHyR/GymHyR/Data/ApplicationDbContext.cs
+++ GymHyR/GymHyR/Data/ApplicationDbContext.cs
@@ -7,5 +7,4 @@ namespace GymHyR.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
-    public DbSet<Producto> Productos { get; set; }
 }
