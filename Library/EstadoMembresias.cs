f019921d7041214f4c5697c49d536c03fa1d6694
HenzoWTF <Urenahenzo@gmail.com>
2024-03-29 11:20:37 -0400

    Librera de randy adaptada con los demás

---
+++ Library/EstadoMembresias.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+
+    public class EstadoMembresias
+    {
+
+        [Key]
+        public int EstadoMembresiaId { get; set; }
+
+        public string? Descripcion { get; set; }
+
+        [ForeignKey("EstadoMembresiaId")]
+        public ICollection<Membresias> Membresias { get; set; } = new List<Membresias>();
+
+    }
+}
---
+++ Library/Membresias.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Membresias
+    {
+        [Key]
+        public int MembresiaId { get; set; }
+
+        public int ClienteId { get; set; }
+
+        public int TipoMembresiaId { get; set; }
+
+        public int EstadoMembresiaId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public DateTime FechaInicio { get; set; } = DateTime.Today;
+        public DateTime FechaFechaFin { get; set; } = DateTime.Today;
+    }
+}
