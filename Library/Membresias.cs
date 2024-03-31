1bf028baf2e20d362f6905b356567ee7d71a9df5
HenzoWTF <Urenahenzo@gmail.com>
2024-03-30 21:10:55 -0400

    Membresias con su Services

--- Library/Membresias.cs
+++ Library/Membresias.cs
@@ -1,25 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Library
 {
-    public class Membresias
-    {
-        [Key]
-        public int MembresiaId { get; set; }
+	public class Membresias
+	{
+		[Key]
+		public int MembresiaId { get; set; }
 
-        public int ClienteId { get; set; }
+		public string? Cedula { get; set; }
 
-        public int TipoMembresiaId { get; set; }
+		public int TipoMembresiaId { get; set; }
 
-        public int EstadoMembresiaId { get; set; }
+		public int EstadoMembresiaId { get; set; }
 
-        [Required(ErrorMessage = "Este campo es requerido")]
-        public DateTime FechaInicio { get; set; } = DateTime.Today;
-        public DateTime FechaFechaFin { get; set; } = DateTime.Today;
-    }
+		public decimal valor { get; set; }
+
+		[Required(ErrorMessage = "Este campo es requerido")]
+		public DateTime FechaInicio { get; set; } = DateTime.Today;
+		public DateTime FechaFechaFin { get; set; } = DateTime.Today;
+
+		[ForeignKey("MembresiaId")]
+		public ICollection<Visitas> Visitas { get; set; } = new List<Visitas>();
+	}
 }
