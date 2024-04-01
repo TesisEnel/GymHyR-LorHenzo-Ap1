76dbe8537cf111eab77ab55e0edd3184a0f7ae3a
HenzoWTF <Urenahenzo@gmail.com>
2024-03-31 23:53:09 -0400

    VentasServices

--- GymHyR/GymHyR/Services/VentasServices.cs
+++ GymHyR/GymHyR/Services/VentasServices.cs
@@ -4,96 +4,92 @@ using Library;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace GymHyR.Services
+namespace GymHyR.Services;
+public class VentasServices(Context context)
 {
-	public class VentasServices(Context context)
-	{
-		public async Task<IEnumerable<Ventas>> GetVentas()
-		{
-			return await context.Venta.Include(p => p.VentaDetalle)
-			.Select(d => new Ventas()
-			{
-				VentaId = d.VentaId,
-				Fecha = d.Fecha,
-				VentaDetalle = d.VentaDetalle
-			}).ToListAsync();
-		}
-		public async Task<Ventas?> GetVenta(int id)
-		{
-			return await context.Venta.Include(p => p.VentaDetalle)
-				.Where(c => c.VentaId == id).FirstOrDefaultAsync();
-		}
+    public async Task<IEnumerable<Ventas>> GetVentas()
+    {
+        return await context.Venta.Include(p => p.VentaDetalle)
+        .Select(d => new Ventas()
+        {
+            VentaId = d.VentaId,
+            Fecha = d.Fecha,
+            VentaDetalle = d.VentaDetalle
+        }).ToListAsync();
+    }
+    public async Task<Ventas?> GetVenta(int id)
+    {
+        return await context.Venta.Include(p => p.VentaDetalle)
+            .Where(c => c.VentaId == id).FirstOrDefaultAsync();
+    }
 
-		public async Task<Ventas> PostCompras(Ventas Venta)
-		{
-			context.Venta.Add(Venta);
-			await context.SaveChangesAsync();
-			return Venta;
-		}
+    public async Task<Ventas> PostVentas(Ventas Venta)
+    {
+        context.Venta.Add(Venta);
+        await context.SaveChangesAsync();
+        return Venta;
+    }
 
-		public async Task<IActionResult> PutCompras(int id, Ventas Venta)
-		{
-			if (id != Venta.VentaId)
-			{
-				return new BadRequestResult();
-			}
+    public async Task<IActionResult> PutCompras(int id, Ventas Venta)
+    {
+        if (id != Venta.VentaId)
+        {
+            return new BadRequestResult();
+        }
 
-			context.Entry(Venta).State = EntityState.Modified;
+        context.Entry(Venta).State = EntityState.Modified;
 
-			try
-			{
-				await context.SaveChangesAsync();
-			}
-			catch (DbUpdateConcurrencyException)
-			{
-				if (!VentaExists(id))
-				{
-					return new NotFoundResult();
-				}
-				else
-				{
-					throw;
-				}
-			}
+        try
+        {
+            await context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!VentaExists(id))
+            {
+                return new NotFoundResult();
+            }
+            else
+            {
+                throw;
+            }
+        }
 
-			return new NoContentResult();
-		}
+        return new NoContentResult();
+    }
 
-		public async Task DeleteVentas(int id)
-		{
-			var Compra = await context.Venta
-				.FirstOrDefaultAsync(p => p.VentaId == id);
+    public async Task DeleteVentas(int id)
+    {
+        var Compra = await context.Venta
+            .FirstOrDefaultAsync(p => p.VentaId == id);
 
-			if (Compra != null)
-			{
-				context.Venta.Remove(Compra);
-				await context.SaveChangesAsync();
-			}
-		}
+        if (Compra != null)
+        {
+            context.Venta.Remove(Compra);
+            await context.SaveChangesAsync();
+        }
+    }
 
-		public async Task DeleteDetalle(int VentaId, int detalleId)
-		{
-			var Venta = await context.Venta
-			.Include(d => d.VentaDetalle)
-				.FirstOrDefaultAsync(p => p.VentaId == VentaId);
+    public async Task DeleteDetalle(int VentaId, int detalleId)
+    {
+        var Venta = await context.Venta
+        .Include(d => d.VentaDetalle)
+            .FirstOrDefaultAsync(p => p.VentaId == VentaId);
 
-			if (Venta != null)
-			{
-				var detalle = Venta.VentaDetalle.FirstOrDefault(d => d.VentaDetalleId == detalleId);
-				if (detalle != null)
-				{
-					Venta.VentaDetalle.Remove(detalle);
-					await context.SaveChangesAsync();
-				}
-			}
-		}
+        if (Venta != null)
+        {
+            var detalle = Venta.VentaDetalle.FirstOrDefault(d => d.VentaDetalleId == detalleId);
+            if (detalle != null)
+            {
+                Venta.VentaDetalle.Remove(detalle);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
 
-		public bool VentaExists(int id)
-		{
-			return context.Venta.Any(e => e.VentaId == id);
-		}
-
-	}
+    public bool VentaExists(int id)
+    {
+        return context.Venta.Any(e => e.VentaId == id);
+    }
 
 }
-
