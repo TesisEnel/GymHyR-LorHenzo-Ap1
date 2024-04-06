3130555e1bb7d0a564c09d533acc91ef69223968
Randy <133064874+Randy-Fabian-Belliard@users.noreply.github.com>
2024-04-06 17:17:13 -0400

    Creando modelo de hprario de entrenadores

---
+++ Library/HorarioEntrenador.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class HorarioEntrenador
+    {
+        [Key]
+        public int HorarioEntrenadorId { get; set; }
+
+        [StringLength(200)]
+        public string Descripcion { get; set; }
+
+
+        [ForeignKey("HorarioEntrenadorId")]
+        public ICollection<Entrenador> Entrenador { get; set; } = new List<Entrenador>();
+
+        [ForeignKey("HorarioEntrenadorId")]
+        public ICollection<CitasEntretamiento> CitasEntretamiento { get; set; } = new List<CitasEntretamiento>();
+    }
+}
