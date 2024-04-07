00ffc569b71a4ecb219a8e353aba5a60bb8793a5
HenzoWTF <Urenahenzo@gmail.com>
2024-04-06 22:36:07 -0400

    Actualizacion de base de datos

---
+++ GymHyR/GymHyR/Migrations/20240407023447_Pedidos.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GymHyR.Migrations
+{
+    /// <inheritdoc />
+    public partial class Pedidos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pedidos",
+                columns: table => new
+                {
+                    PedidoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FechaPedido = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ClienteNombre = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pedidos", x => x.PedidoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PedidoDetalle",
+                columns: table => new
+                {
+                    PedidoDetalleId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductoId = table.Column<int>(type: "int", nullable: false),
+                    ProductoNombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PrecioUnitario = table.Column<float>(type: "real", nullable: false),
+                    Cantidad = table.Column<int>(type: "int", nullable: false),
+                    PedidoId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PedidoDetalle", x => x.PedidoDetalleId);
+                    table.ForeignKey(
+                        name: "FK_PedidoDetalle_Pedidos_PedidoId",
+                        column: x => x.PedidoId,
+                        principalTable: "Pedidos",
+                        principalColumn: "PedidoId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PedidoDetalle_PedidoId",
+                table: "PedidoDetalle",
+                column: "PedidoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PedidoDetalle");
+
+            migrationBuilder.DropTable(
+                name: "Pedidos");
+        }
+    }
+}
--- Library/Pedidos.cs
+++ Library/Pedidos.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,12 @@ namespace Library;
 
 public class Pedidos
 {
+    [Key]
     public int PedidoId { get; set; }
     public DateTime FechaPedido { get; set; }
     public string ClienteNombre { get; set; }
+
+
+    [ForeignKey("PedidoId")]
     public List<PedidoDetalle> Detalles { get; set; } = new List<PedidoDetalle>();
 }
