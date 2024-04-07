0149beda06904e7a3c9866249e99a57a12fb7377
Randy <133064874+Randy-Fabian-Belliard@users.noreply.github.com>
2024-04-06 21:16:18 -0400

    Creando modelo de citas

---
+++ Library/CitasEntrenamiento.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class CitasEntrenamiento
+    {
+        [Key]
+        public int CitasEntrenamientoId { get; set; }
+
+        public int EntrenadorId { get; set; }
+
+        public DateTime FechaCita { get; set; }
+
+        public TimeSpan HoraCita { get; set; }
+
+        
+
+    }
+}
