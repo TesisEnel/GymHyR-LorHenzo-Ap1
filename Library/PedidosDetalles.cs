7893094422849ca5b9e7510ba1f8b1391e151bac
HenzoWTF <Urenahenzo@gmail.com>
2024-04-06 22:25:17 -0400

    Clzse pedidosDetalles

---
+++ Library/PedidosDetalles.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library;
+
+public class PedidoDetalle
+{
+    public int PedidoDetalleId { get; set; }
+    public int ProductoId { get; set; }
+    public string ProductoNombre { get; set; }
+    public decimal PrecioUnitario { get; set; }
+    public int Cantidad { get; set; }
+    public decimal Total { get { return PrecioUnitario * Cantidad; } } // Calculated property
+}
