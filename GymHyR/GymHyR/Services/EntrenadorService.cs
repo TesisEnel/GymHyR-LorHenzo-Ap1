6912b3525a3f1f262211fb16e37b8f023c9cca72
Randy <133064874+Randy-Fabian-Belliard@users.noreply.github.com>
2024-04-06 21:15:26 -0400

    Creando el services del entrenador

---
+++ GymHyR/GymHyR/Services/EntrenadorService.cs
@@ -0,0 +1,82 @@
+﻿using GymHyR.Data;
+using Library;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymHyR.Services
+{
+    public class EntrenadorService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EntrenadorService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Entrenador>> GetEntrenadores()
+        {
+            return await _context.Entrenador.ToListAsync();
+        }
+
+        public async Task<Entrenador?> GetEntrenador(int id)
+        {
+            return await _context.Entrenador.FindAsync(id);
+        }
+
+        public async Task<Entrenador> PostEntrenador(Entrenador entrenador)
+        {
+            _context.Entrenador.Add(entrenador);
+            await _context.SaveChangesAsync();
+            return entrenador;
+        }
+
+        public async Task<IActionResult> PutEntrenador(int id, Entrenador entrenador)
+        {
+            if (id != entrenador.EntrenadorId)
+            {
+                return new BadRequestResult();
+            }
+
+            _context.Entry(entrenador).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EntrenadorExists(id))
+                {
+                    return new NotFoundResult();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return new NoContentResult();
+        }
+
+        public async Task DeleteEntrenador(int id)
+        {
+            var entrenador = await _context.Entrenador.FindAsync(id);
+
+            if (entrenador != null)
+            {
+                _context.Entrenador.Remove(entrenador);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public bool EntrenadorExists(int id)
+        {
+            return _context.Entrenador.Any(e => e.EntrenadorId == id);
+        }
+    }
+}
