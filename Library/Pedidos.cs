d4f7a6adfede74a3d39fe6c8bc091770dcf8a2b1
HenzoWTF <Urenahenzo@gmail.com>
2024-04-06 22:16:30 -0400

    Clase pedido realizada

---
+++ Library/Pedidos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library;
+
+public class Pedido
+{
+    public int PedidoId { get; set; }
+    public DateTime FechaPedido { get; set; }
+    public string ClienteNombre { get; set; }
+    public List<PedidoDetalle> Detalles { get; set; } = new List<PedidoDetalle>();
+}
