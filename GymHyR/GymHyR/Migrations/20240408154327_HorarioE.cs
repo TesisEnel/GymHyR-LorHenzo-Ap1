582d349666bd646560f6f2499a3172ff3c146675
HenzoWTF <Urenahenzo@gmail.com>
2024-04-08 11:44:38 -0400

    Actualizacion base de datos

---
+++ GymHyR/GymHyR/Migrations/20240408154327_HorarioE.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GymHyR.Migrations
+{
+    /// <inheritdoc />
+    public partial class HorarioE : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "HorarioEntrenadorId",
+                table: "CitasEntrenamiento",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "HorarioEntrenadorId",
+                table: "CitasEntrenamiento",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
